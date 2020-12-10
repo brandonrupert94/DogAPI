c31fb2c5773cd939499daca1d76dda47400990fb
Brandon Rupert <brupert94k@gmail.com>
2020-12-09 23:48:53 -0600

    final changes

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ function displayResults(responseJson){
 function getImage() {
     let imgAmount = document.getElementById('amount').value;
     let url = "https://dog.ceo/api/breeds/image/random/" + imgAmount;
-    
+    $('#results').show;
     fetch(url)
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson))
         .catch(error => alert('Something went wrong. Try again later'));
+
+    
 }
 
 function handleSubmit() {
     $('form').submit(event => {
         event.preventDefault();
         getImage();
+        
     })
 };
 
