d2b156044630425db6e2abe64e0b4ac080b23261
Brandon Rupert <brupert94k@gmail.com>
2020-12-09 23:32:28 -0600

    fixed function to call correct amount of photos

--- app.js
+++ app.js
@@ -2,25 +2,19 @@ function displayResults(responseJson){
     console.log(responseJson.message);
 }
 function getImage() {
-    fetch('https://dog.ceo/api/breeds/image/random/3')
+    let imgAmount = document.getElementById('amount').value;
+    let url = "https://dog.ceo/api/breeds/image/random/" + imgAmount;
+    
+    fetch(url)
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson))
         .catch(error => alert('Something went wrong. Try again later'));
 }
 
-function getAmountOfImages() {
-    
-    let imgAmount = document.getElementById('amount').value;
-    console.log(imgAmount);
-}
-
-
-
 function handleSubmit() {
     $('form').submit(event => {
         event.preventDefault();
         getImage();
-        getAmountOfImages();
     })
 };
 
